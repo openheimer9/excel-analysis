89e7a12d1c2f137ffc8c3b5c43c2b45af80ecb02
openheimer9 <147526211+openheimer9@users.noreply.github.com>
2025-06-25 13:34:37 +0530

    Update FileHistory.js retry

--- frontend/src/components/files/FileHistory.js
+++ frontend/src/components/files/FileHistory.js
@@ -83,7 +83,7 @@ const FileHistory = () => {
         responseType: "blob",
       });
       
-      // Create a download link
+      // Create a download link 
       const url = window.URL.createObjectURL(new Blob([response.data]));
       const link = document.createElement("a");
       link.href = url;
